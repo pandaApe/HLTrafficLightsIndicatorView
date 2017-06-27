a409bd2f3733dd1fa1509a9711965152a3d002be
Wang HaiLong <whailong2010@gmail.com>
2017-06-27 22:28:06 +0800

    Removed Pods folder

--- Example/Pods/Target Support Files/HLTrafficLightsIndicatorView/HLTrafficLightsIndicatorView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HLTrafficLightsIndicatorView : NSObject
-@end
-@implementation PodsDummy_HLTrafficLightsIndicatorView
-@end
--- Example/Pods/Target Support Files/Pods-HLTrafficLightsIndicatorView_Tests/Pods-HLTrafficLightsIndicatorView_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HLTrafficLightsIndicatorView_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_HLTrafficLightsIndicatorView_Tests
-@end
